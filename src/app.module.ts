9251eb35a89cdad2a13207e6b335c7632f170d16
SobolevSerhii <sobolevsergeysom@gmail.com>
2021-05-23 23:16:43 +0300

    Add typeorm module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
 import { Module } from '@nestjs/common';
-import { GraphQLModule } from '@nestjs/graphql';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { GraphQLModule } from '@nestjs/graphql';
+import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
-    GraphQLModule.forRoot({})
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql'
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '',
+      database: 'invoiceapp',
+      entities: ['dist/**/*.model.js'],
+      synchronize: false,
+    })
   ],
   controllers: [AppController],
   providers: [AppService],
